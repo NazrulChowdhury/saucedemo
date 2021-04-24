422bbde373a97d69fe50011f7c0d04b29f9dab47
Nazrul Chowdhury <nazrulchowdhury404@gmail.com>
2021-04-24 23:53:14 +0100

    credentials added

---
+++ cypress/integration/UI/login.spec.js
@@ -0,0 +1,8 @@
+describe('Sign in test with correct and incorrect credentials', ()=>{
+    beforeEach(()=>{
+        cy.visit('/')
+    })
+    it('Log in attempt successful with correct credentials', ()=>{
+        cy.get()
+    })
+})
