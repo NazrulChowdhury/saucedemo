990c44c34262b9b50d53a840d03699adb64bc221
Nazrul Chowdhury <nazrulchowdhury404@gmail.com>
2021-04-25 01:36:40 +0100

    updated

--- cypress/integration/UI/login.spec.js
+++ cypress/integration/UI/login.spec.js
@@ -1,8 +1,14 @@
+import user from "../../fixtures/userInfo.json"
+
 describe('Sign in test with correct and incorrect credentials', ()=>{
     beforeEach(()=>{
         cy.visit('/')
     })
     it('Log in attempt successful with correct credentials', ()=>{
-        cy.get()
+        cy.get('#user-name').type(user.name)
+        cy.get('#password').type(user.password)
+        cy.get('input[type = "submit"]').should('have.text', 'LOGIN ').click()
+        cy.url().should('include', "/inventory")
+        cy.title().should('eq', 'Swag Labs')
     })
 })
