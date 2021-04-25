c3101bbbf6a285a3f12bcce9a24ecd6fb7ce6baf
Nazrul Chowdhury <nazrulchowdhury404@gmail.com>
2021-04-25 02:20:37 +0100

    added login spec

--- cypress/integration/UI/login.spec.js
+++ cypress/integration/UI/login.spec.js
@@ -4,11 +4,19 @@ describe('Sign in test with correct and incorrect credentials', ()=>{
     beforeEach(()=>{
         cy.visit('/')
     })
-    it('Log in attempt successful with correct credentials', ()=>{
-        cy.get('#user-name').type(user.name)
+    it('sign in attempt successful with correct credentials', ()=>{
+        cy.signIn(user)
+    })
+    it('sign in attempt fails with wrong username',()=>{
+        cy.get('#user-name').type(user.wrongName)
         cy.get('#password').type(user.password)
-        cy.get('input[type = "submit"]').should('have.text', 'LOGIN ').click()
-        cy.url().should('include', "/inventory")
-        cy.title().should('eq', 'Swag Labs')
+        cy.get('#login-button').click()
+        cy.get('.error-message-container').contains('Epic sadface:')
+    })
+    it('sign in attempt fails with wrong password',()=>{
+        cy.get('#user-name').type(user.name)
+        cy.get('#password').type(user.wrongPassword)
+        cy.get('#login-button').click()
+        cy.get('.error-message-container').contains('Epic sadface:')
     })
 })
